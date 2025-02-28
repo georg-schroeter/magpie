8148c0f0ffaa44e1d178f125f7fcbeaca94b39d1
Georg Schroeter <Georg.Schroeter@pik-potsdam.de>
2025-02-28 12:21:07 +0100

    generate csv files for experiment 3

--- land_conversion_sensitivity/generate_f39calibcsv.R
+++ land_conversion_sensitivity/generate_f39calibcsv.R
@@ -1,8 +1,16 @@
-data <- read.csv("input/test001_reference.csv", sep=",", comment.char="*", dec=".")
+data <- read.csv("input/test000_reference.csv", sep=",", comment.char="*", dec=".")
 
 for (region in list("CAZ", "CHA", "EUR", "IND", "JPN", "LAM", "MEA", "NEU", "OAS", "REF", "SSA", "USA", "all")) {
 cat(region, " ")
-  for (factor in list (0.5,0.625,0.8,1.25,1.6,2.0)) {
+  # generate reference file
+  data[[3]] <- 1
+  data[[4]] <- 0
+  file_name <- paste0("input/test002_reference.csv")
+  write.csv(data,file_name,row.names=FALSE)
+  
+  
+  # for (factor in list (0.5,0.625,0.8,1.25,1.6,2.0)) {
+  for (factor in list (0.1,0.2,0.4,0.7,1.0)) {
     data[[3]] <- 1
     data[[4]] <- 0
     
@@ -12,9 +20,9 @@ cat(region, " ")
       matches <- (data[[2]] == region)
     }
     
-    data[[3]][matches] <- factor
-    # data[[4]][matches] <- 0
-    file_name <- paste0("input/test001_", region, "_", factor, ".csv")
+    # data[[3]][matches] <- factor
+    data[[4]][matches] <- factor
+    file_name <- paste0("input/test002_", region, "_", factor, ".csv")
     write.csv(data,file_name,row.names=FALSE)
   }
 }
